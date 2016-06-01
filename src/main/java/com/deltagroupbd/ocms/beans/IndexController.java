edfad8d705758dac57b9ad1c0835eb49f138ef7f
Md.Shamim Miah <shamim.ict0754@gmail.com>
2016-06-01 16:09:50 +0600

    test commit

--- src/main/java/com/deltagroupbd/ocms/beans/IndexController.java
+++ src/main/java/com/deltagroupbd/ocms/beans/IndexController.java
@@ -5,12 +5,12 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "index",eager = true)
 @RequestScoped
-public class Index {
-   public Index() {
+public class IndexController {
+   public IndexController() {
       System.out.println("it is printed before any request come since eager = true");
    }
 	
    public String getMessage() {
-      return "Welcome to delta group ocms";
+      return "Welcome to delta group ocms 3";
    }
 }
